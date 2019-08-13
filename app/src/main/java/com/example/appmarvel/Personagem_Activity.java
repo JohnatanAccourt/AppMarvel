3ca84df21a5079e94ce7c400364fe3a2cc20d6aa
johnatan.asantos <johnatan1999accourt@gmail.com>
2019-08-13 17:04:37 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appmarvel/Personagem_Activity.java
@@ -0,0 +1,14 @@
+package com.example.appmarvel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Personagem_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personagem_layout);
+    }
+}
